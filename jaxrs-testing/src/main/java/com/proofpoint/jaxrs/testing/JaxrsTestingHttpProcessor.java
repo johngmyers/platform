airlift/airlift

Add jaxrs-testing module
It adds an implementation of TestingHttpClient.Processor that delegates to
JAX-RS resources.

jaxrs-testing/src/main/java/io/airlift/jaxrs/testing/JaxrsTestingHttpProcessor.java
@@ -1,0 +1,136 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.airlift.jaxrs.testing;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.ImmutableSet;
+import io.airlift.http.client.HttpStatus;
+import io.airlift.http.client.Request;
+import io.airlift.http.client.Response;
+import io.airlift.http.client.testing.TestingHttpClient;
+import io.airlift.http.client.testing.TestingResponse;
+import io.airlift.log.Logger;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.glassfish.jersey.test.DeploymentContext;
+import org.glassfish.jersey.test.inmemory.InMemoryTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainer;
+
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+
+public class JaxrsTestingHttpProcessor
+        implements TestingHttpClient.Processor
+{
+    private static final Logger log = Logger.get(JaxrsTestingHttpProcessor.class);
+
+    private final Client client;
+
+    private boolean trace;
+
+    public JaxrsTestingHttpProcessor(URI baseUri, Object... jaxRsSingletons)
+    {
+        Set<Object> jaxRsSingletonsSet = ImmutableSet.copyOf(jaxRsSingletons);
+        Application application = new Application() {
+            @Override
+            public Set<Object> getSingletons()
+            {
+                return jaxRsSingletonsSet;
+            }
+        };
+        TestContainer testContainer = new InMemoryTestContainerFactory()
+                .create(baseUri, DeploymentContext.newInstance(application));
+        ClientConfig clientConfig = testContainer.getClientConfig();
+        this.client = JerseyClientBuilder.createClient(clientConfig);
+    }
+
+    public JaxrsTestingHttpProcessor setTrace(boolean enabled)
+    {
+        this.trace = enabled;
+        return this;
+    }
+
+    @Override
+    public Response handle(Request request)
+            throws Exception
+    {
+        // prepare request to jax-rs resource
+        MultivaluedMap<String, Object> requestHeaders = new MultivaluedHashMap<>();
+        for (Map.Entry<String, String> entry : request.getHeaders().entries()) {
+            requestHeaders.add(entry.getKey(), entry.getValue());
+        }
+        Invocation.Builder invocationBuilder = client.target(request.getUri()).request().headers(requestHeaders);
+        Invocation invocation;
+        if (request.getBodyGenerator() == null) {
+            invocation = invocationBuilder.build(request.getMethod());
+        }
+        else {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            request.getBodyGenerator().write(byteArrayOutputStream);
+            byteArrayOutputStream.close();
+            byte[] bytes = byteArrayOutputStream.toByteArray();
+            Entity<byte[]> entity = Entity.entity(bytes, (String) getOnlyElement(requestHeaders.get("Content-Type")));
+            invocation = invocationBuilder.build(request.getMethod(), entity);
+        }
+
+        // issue request, and handle exceptions
+        javax.ws.rs.core.Response result;
+        try {
+            result = invocation.invoke(javax.ws.rs.core.Response.class);
+        }
+        catch (ProcessingException exception) {
+            if (trace) {
+                log.warn(exception.getCause(), "%-8s %s -> Exception", request.getMethod(), request.getUri());
+            }
+            // Peel out the exception we threw in jax-rs resource implementation
+            // to facilitate testing exceptional paths
+            if (exception.getCause() instanceof Exception) {
+                throw (Exception) exception.getCause();
+            }
+            throw exception;
+        }
+        catch (Throwable throwable) {
+            if (trace) {
+                log.warn(throwable, "%-8s %s -> Fail", request.getMethod(), request.getUri());
+            }
+            throw throwable;
+        }
+
+        // process response from jax-rs resource
+        ImmutableListMultimap.Builder<String, String> responseHeaders = ImmutableListMultimap.builder();
+        for (Map.Entry<String, List<String>> headerEntry : result.getStringHeaders().entrySet()) {
+            for (String value : headerEntry.getValue()) {
+                responseHeaders.put(headerEntry.getKey(), value);
+            }
+        }
+
+        if (trace) {
+            log.warn("%-8s %s -> OK", request.getMethod(), request.getUri());
+        }
+        return new TestingResponse(HttpStatus.OK, responseHeaders.build(), result.readEntity(byte[].class));
+    }
+}

jaxrs-testing/src/test/java/io/airlift/jaxrs/testing/TestJaxrsTestingHttpProcessor.java
@@ -1,0 +1,85 @@
+package io.airlift.jaxrs.testing;
+
+import io.airlift.http.client.HttpStatus;
+import io.airlift.http.client.Request;
+import io.airlift.http.client.StringResponseHandler.StringResponse;
+import io.airlift.http.client.testing.TestingHttpClient;
+import org.testng.annotations.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.net.URI;
+
+import static io.airlift.http.client.Request.Builder.prepareGet;
+import static io.airlift.http.client.StringResponseHandler.createStringResponseHandler;
+import static java.lang.String.format;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+@Test
+public class TestJaxrsTestingHttpProcessor
+{
+    private static final TestingHttpClient HTTP_CLIENT =
+            new TestingHttpClient(new JaxrsTestingHttpProcessor(URI.create("http://fake.invalid/"), new GetItResource()));
+
+    @Test
+    public void test()
+            throws Exception
+    {
+        Request request = prepareGet()
+                .setUri(URI.create("http://fake.invalid/get-it/get/xyz"))
+                .build();
+
+        StringResponse response = HTTP_CLIENT.execute(request, createStringResponseHandler());
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK.code());
+        assertEquals(response.getBody(), "Got xyz");
+    }
+
+    @Test
+    public void testException()
+            throws Exception
+    {
+        Request request = prepareGet()
+                .setUri(URI.create("http://fake.invalid/get-it/fail/testException"))
+                .build();
+
+        try {
+            HTTP_CLIENT.execute(request, createStringResponseHandler());
+            fail("expected exception");
+        }
+        catch (TestingException e) {
+            assertEquals(e.getMessage(), "testException");
+        }
+    }
+
+    @Path("get-it")
+    public static class GetItResource
+    {
+        @Path("get/{id}")
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public String getId(@PathParam("id") String id)
+        {
+            return format("Got %s", id);
+        }
+
+        @Path("fail/{message}")
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public String fail(@PathParam("message") String errorMessage)
+        {
+            throw new TestingException(errorMessage);
+        }
+    }
+
+    private static class TestingException extends RuntimeException
+    {
+        public TestingException(String message) {
+            super(message);
+        }
+    }
+}

johngmyers/platform

jaxrs-testing/src/main/java/com/proofpoint/jaxrs/testing/JaxrsTestingHttpProcessor.java
@@ -1,0 +1,139 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.proofpoint.jaxrs.testing;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.ImmutableSet;
+import com.proofpoint.http.client.Request;
+import com.proofpoint.http.client.Response;
+import com.proofpoint.http.client.testing.TestingHttpClient;
+import com.proofpoint.log.Logger;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.glassfish.jersey.test.DeploymentContext;
+import org.glassfish.jersey.test.inmemory.InMemoryTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainer;
+
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static com.proofpoint.http.client.testing.BodySourceTester.writeBodySourceTo;
+import static com.proofpoint.http.client.testing.TestingResponse.mockResponse;
+
+public class JaxrsTestingHttpProcessor
+        implements TestingHttpClient.Processor
+{
+    private static final Logger log = Logger.get(JaxrsTestingHttpProcessor.class);
+
+    private final Client client;
+
+    private boolean trace;
+
+    public JaxrsTestingHttpProcessor(URI baseUri, Object... jaxRsSingletons)
+    {
+        Set<Object> jaxRsSingletonsSet = ImmutableSet.copyOf(jaxRsSingletons);
+        Application application = new Application() {
+            @Override
+            public Set<Object> getSingletons()
+            {
+                return jaxRsSingletonsSet;
+            }
+        };
+        TestContainer testContainer = new InMemoryTestContainerFactory()
+                .create(baseUri, DeploymentContext.newInstance(application));
+        ClientConfig clientConfig = testContainer.getClientConfig();
+        this.client = JerseyClientBuilder.createClient(clientConfig);
+    }
+
+    public JaxrsTestingHttpProcessor setTrace(boolean enabled)
+    {
+        this.trace = enabled;
+        return this;
+    }
+
+    @Override
+    public Response handle(Request request)
+            throws Exception
+    {
+        // prepare request to jax-rs resource
+        MultivaluedMap<String, Object> requestHeaders = new MultivaluedHashMap<>();
+        for (Map.Entry<String, String> entry : request.getHeaders().entries()) {
+            requestHeaders.add(entry.getKey(), entry.getValue());
+        }
+        Invocation.Builder invocationBuilder = client.target(request.getUri()).request().headers(requestHeaders);
+        Invocation invocation;
+        if (request.getBodySource() == null) {
+            invocation = invocationBuilder.build(request.getMethod());
+        }
+        else {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            writeBodySourceTo(request.getBodySource(), byteArrayOutputStream);
+            byteArrayOutputStream.close();
+            byte[] bytes = byteArrayOutputStream.toByteArray();
+            Entity<byte[]> entity = Entity.entity(bytes, (String) getOnlyElement(requestHeaders.get("Content-Type")));
+            invocation = invocationBuilder.build(request.getMethod(), entity);
+        }
+
+        // issue request, and handle exceptions
+        javax.ws.rs.core.Response result;
+        try {
+            result = invocation.invoke(javax.ws.rs.core.Response.class);
+        }
+        catch (ProcessingException exception) {
+            if (trace) {
+                log.warn(exception.getCause(), "%-8s %s -> Exception", request.getMethod(), request.getUri());
+            }
+            // Peel out the exception we threw in jax-rs resource implementation
+            // to facilitate testing exceptional paths
+            if (exception.getCause() instanceof Exception) {
+                throw (Exception) exception.getCause();
+            }
+            throw exception;
+        }
+        catch (Throwable throwable) {
+            if (trace) {
+                log.warn(throwable, "%-8s %s -> Fail", request.getMethod(), request.getUri());
+            }
+            throw throwable;
+        }
+
+        // process response from jax-rs resource
+        ImmutableListMultimap.Builder<String, String> responseHeaders = ImmutableListMultimap.builder();
+        for (Map.Entry<String, List<String>> headerEntry : result.getStringHeaders().entrySet()) {
+            for (String value : headerEntry.getValue()) {
+                responseHeaders.put(headerEntry.getKey(), value);
+            }
+        }
+
+        if (trace) {
+            log.warn("%-8s %s -> OK", request.getMethod(), request.getUri());
+        }
+        return mockResponse()
+                .headers(responseHeaders.build())
+                .body(result.readEntity(byte[].class))
+                .build();
+    }
+}

jaxrs-testing/test/java/com/proofpoint/jaxrs/testing/TestJaxrsTestingHttpProcessor.java
@@ -1,0 +1,85 @@
+package com.proofpoint.jaxrs.testing;
+
+import com.proofpoint.http.client.HttpStatus;
+import com.proofpoint.http.client.Request;
+import com.proofpoint.http.client.StringResponseHandler.StringResponse;
+import com.proofpoint.http.client.testing.TestingHttpClient;
+import org.testng.annotations.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.net.URI;
+
+import static com.proofpoint.http.client.Request.Builder.prepareGet;
+import static com.proofpoint.http.client.StringResponseHandler.createStringResponseHandler;
+import static java.lang.String.format;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+@Test
+public class TestJaxrsTestingHttpProcessor
+{
+    private static final TestingHttpClient HTTP_CLIENT =
+            new TestingHttpClient(new JaxrsTestingHttpProcessor(URI.create("http://fake.invalid/"), new GetItResource()));
+
+    @Test
+    public void test()
+            throws Exception
+    {
+        Request request = prepareGet()
+                .setUri(URI.create("http://fake.invalid/get-it/get/xyz"))
+                .build();
+
+        StringResponse response = HTTP_CLIENT.execute(request, createStringResponseHandler());
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK.code());
+        assertEquals(response.getBody(), "Got xyz");
+    }
+
+    @Test
+    public void testException()
+            throws Exception
+    {
+        Request request = prepareGet()
+                .setUri(URI.create("http://fake.invalid/get-it/fail/testException"))
+                .build();
+
+        try {
+            HTTP_CLIENT.execute(request, createStringResponseHandler());
+            fail("expected exception");
+        }
+        catch (TestingException e) {
+            assertEquals(e.getMessage(), "testException");
+        }
+    }
+
+    @Path("get-it")
+    public static class GetItResource
+    {
+        @Path("get/{id}")
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public String getId(@PathParam("id") String id)
+        {
+            return format("Got %s", id);
+        }
+
+        @Path("fail/{message}")
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public String fail(@PathParam("message") String errorMessage)
+        {
+            throw new TestingException(errorMessage);
+        }
+    }
+
+    private static class TestingException extends RuntimeException
+    {
+        public TestingException(String message) {
+            super(message);
+        }
+    }
+}