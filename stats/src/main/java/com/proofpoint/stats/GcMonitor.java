airlift/airlift

Add JmxGcMonitor to track GC events

stats/src/main/java/io/airlift/stats/GcMonitor.java
@@ -1,0 +1,23 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.stats;
+
+import io.airlift.units.Duration;
+
+public interface GcMonitor
+{
+    long getMajorGcCount();
+
+    Duration getMajorGcTime();
+}

stats/src/main/java/io/airlift/stats/TestingGcMonitor.java
@@ -1,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.stats;
+
+import io.airlift.units.Duration;
+
+import javax.annotation.concurrent.GuardedBy;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+public class TestingGcMonitor
+        implements GcMonitor
+{
+    @GuardedBy("this")
+    private long majorGcCount;
+
+    @GuardedBy("this")
+    private long majorGcTimeNanos;
+
+    public synchronized void recordMajorGc(Duration duration)
+    {
+        majorGcCount++;
+        majorGcTimeNanos += duration.roundTo(NANOSECONDS);
+    }
+
+    @Override
+    public synchronized long getMajorGcCount()
+    {
+        return majorGcCount;
+    }
+
+    @Override
+    public synchronized Duration getMajorGcTime()
+    {
+        return new Duration(majorGcTimeNanos, NANOSECONDS);
+    }
+}

stats/src/test/java/io/airlift/stats/GcMonitorTester.java
@@ -1,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.stats;
+
+import io.airlift.log.Logging;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Manual tester for GcMonitor.
+ * <p>
+ * Test should always be run with:
+ * <pre>
+ * {@code   -Xmx1g -Xms1g -XX:+PrintGCApplicationStoppedTime}
+ * </pre>
+ * All GC algorithms should be tested:
+ * <ul>
+ * <li>Serial: {@code -XX:+UseSerialGC }</li>
+ * <li>Parallel: {@code -XX:+UseParallelGC}</li>
+ * <li>CMS: {@code -XX:+UseConcMarkSweepGC}</li>
+ * <li>G1: {@code -XX:+UseG1GC}</li>
+ * </ul>
+ * Verifying stopped time in GC log to the stopped time from GCMonitor.
+ */
+public final class GcMonitorTester
+{
+    private static final List<byte[]> VALUES = new ArrayList<>();
+    private static final int VALUE_SIZE = 100 * 1024;
+    private static final long MAX_MEMORY_SIZE = (long) (0.9 * 1024 * 1024 * 1024);
+    private static long memorySize;
+
+    private GcMonitorTester() {}
+
+    public static void main(String[] args)
+            throws InterruptedException
+    {
+        Logging.initialize();
+        JmxGcMonitor gcMonitor = new JmxGcMonitor();
+        gcMonitor.start();
+
+        while (true) {
+            byte[] value = new byte[VALUE_SIZE];
+            if (memorySize > MAX_MEMORY_SIZE) {
+                VALUES.set(ThreadLocalRandom.current().nextInt(VALUES.size()), value);
+            }
+            else {
+                VALUES.add(value);
+                memorySize += VALUE_SIZE;
+            }
+            Thread.sleep(1);
+        }
+    }
+}

stats/src/test/java/io/airlift/stats/TestJmxGcMonitor.java
@@ -1,0 +1,28 @@
+package io.airlift.stats;
+
+import io.airlift.units.Duration;
+import org.testng.annotations.Test;
+
+import static io.airlift.testing.Assertions.assertGreaterThanOrEqual;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static org.testng.Assert.assertEquals;
+
+public class TestJmxGcMonitor
+{
+    @Test
+    public void test()
+            throws Exception
+    {
+        JmxGcMonitor gcMonitor = new JmxGcMonitor();
+        assertEquals(gcMonitor.getMajorGcCount(), 0);
+        assertEquals(gcMonitor.getMajorGcTime(), new Duration(0, NANOSECONDS));
+        try {
+            gcMonitor.start();
+            assertGreaterThanOrEqual(gcMonitor.getMajorGcCount(), (long) 0);
+            assertGreaterThanOrEqual(gcMonitor.getMajorGcTime(), new Duration(0, NANOSECONDS));
+        }
+        finally {
+            gcMonitor.stop();
+        }
+    }
+}

stats/src/test/java/io/airlift/stats/TestTestingGcMonitor.java
@@ -1,0 +1,31 @@
+
+package io.airlift.stats;
+
+import io.airlift.units.Duration;
+import org.testng.annotations.Test;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.testng.Assert.assertEquals;
+
+public class TestTestingGcMonitor
+{
+    @Test
+    public void test()
+            throws Exception
+    {
+        TestingGcMonitor gcMonitor = new TestingGcMonitor();
+
+        assertEquals(gcMonitor.getMajorGcCount(), 0);
+        assertEquals(gcMonitor.getMajorGcTime(), new Duration(0, SECONDS));
+
+        gcMonitor.recordMajorGc(new Duration(3, SECONDS));
+
+        assertEquals(gcMonitor.getMajorGcCount(), 1);
+        assertEquals(gcMonitor.getMajorGcTime(), new Duration(3, SECONDS));
+
+        gcMonitor.recordMajorGc(new Duration(7, SECONDS));
+
+        assertEquals(gcMonitor.getMajorGcCount(), 2);
+        assertEquals(gcMonitor.getMajorGcTime(), new Duration(10, SECONDS));
+    }
+}

johngmyers/platform

stats/src/main/java/com/proofpoint/stats/GcMonitor.java
@@ -1,0 +1,24 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.stats;
+
+import com.proofpoint.units.Duration;
+
+public interface GcMonitor
+{
+    long getMajorGcCount();
+
+    Duration getMajorGcTime();
+}
+

stats/src/main/java/com/proofpoint/stats/TestingGcMonitor.java
@@ -1,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.stats;
+
+import com.proofpoint.units.Duration;
+
+import javax.annotation.concurrent.GuardedBy;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+public class TestingGcMonitor
+        implements GcMonitor
+{
+    @GuardedBy("this")
+    private long majorGcCount;
+
+    @GuardedBy("this")
+    private long majorGcTimeNanos;
+
+    public synchronized void recordMajorGc(Duration duration)
+    {
+        majorGcCount++;
+        majorGcTimeNanos += duration.roundTo(NANOSECONDS);
+    }
+
+    @Override
+    public synchronized long getMajorGcCount()
+    {
+        return majorGcCount;
+    }
+
+    @Override
+    public synchronized Duration getMajorGcTime()
+    {
+        return new Duration(majorGcTimeNanos, NANOSECONDS);
+    }
+}
+

stats/src/test/java/com/proofpoint/stats/GcMonitorTester.java
@@ -1,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.stats;
+
+import com.proofpoint.log.Logging;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Manual tester for GcMonitor.
+ * <p>
+ * Test should always be run with:
+ * <pre>
+ * {@code   -Xmx1g -Xms1g -XX:+PrintGCApplicationStoppedTime}
+ * </pre>
+ * All GC algorithms should be tested:
+ * <ul>
+ * <li>Serial: {@code -XX:+UseSerialGC }</li>
+ * <li>Parallel: {@code -XX:+UseParallelGC}</li>
+ * <li>CMS: {@code -XX:+UseConcMarkSweepGC}</li>
+ * <li>G1: {@code -XX:+UseG1GC}</li>
+ * </ul>
+ * Verifying stopped time in GC log to the stopped time from GCMonitor.
+ */
+public final class GcMonitorTester
+{
+    private static final List<byte[]> VALUES = new ArrayList<>();
+    private static final int VALUE_SIZE = 100 * 1024;
+    private static final long MAX_MEMORY_SIZE = (long) (0.9 * 1024 * 1024 * 1024);
+    private static long memorySize;
+
+    private GcMonitorTester() {}
+
+    public static void main(String[] args)
+            throws InterruptedException
+    {
+        Logging.initialize();
+        JmxGcMonitor gcMonitor = new JmxGcMonitor();
+        gcMonitor.start();
+
+        while (true) {
+            byte[] value = new byte[VALUE_SIZE];
+            if (memorySize > MAX_MEMORY_SIZE) {
+                VALUES.set(ThreadLocalRandom.current().nextInt(VALUES.size()), value);
+            }
+            else {
+                VALUES.add(value);
+                memorySize += VALUE_SIZE;
+            }
+            Thread.sleep(1);
+        }
+    }
+}
+

stats/src/test/java/com/proofpoint/stats/TestJmxGcMonitor.java
@@ -1,0 +1,28 @@
+package com.proofpoint.stats;
+
+import com.proofpoint.units.Duration;
+import org.testng.annotations.Test;
+
+import static com.proofpoint.testing.Assertions.assertGreaterThanOrEqual;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static org.testng.Assert.assertEquals;
+
+public class TestJmxGcMonitor
+{
+    @Test
+    public void test()
+            throws Exception
+    {
+        JmxGcMonitor gcMonitor = new JmxGcMonitor();
+        assertEquals(gcMonitor.getMajorGcCount(), 0);
+        assertEquals(gcMonitor.getMajorGcTime(), new Duration(0, NANOSECONDS));
+        try {
+            gcMonitor.start();
+            assertGreaterThanOrEqual(gcMonitor.getMajorGcCount(), (long) 0);
+            assertGreaterThanOrEqual(gcMonitor.getMajorGcTime(), new Duration(0, NANOSECONDS));
+        }
+        finally {
+            gcMonitor.stop();
+        }
+    }
+}

stats/src/test/java/com/proofpoint/stats/TestTestingGcMonitor.java
@@ -1,0 +1,30 @@
+package com.proofpoint.stats;
+
+import com.proofpoint.units.Duration;
+import org.testng.annotations.Test;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.testng.Assert.assertEquals;
+
+public class TestTestingGcMonitor
+{
+    @Test
+    public void test()
+            throws Exception
+    {
+        TestingGcMonitor gcMonitor = new TestingGcMonitor();
+
+        assertEquals(gcMonitor.getMajorGcCount(), 0);
+        assertEquals(gcMonitor.getMajorGcTime(), new Duration(0, SECONDS));
+
+        gcMonitor.recordMajorGc(new Duration(3, SECONDS));
+
+        assertEquals(gcMonitor.getMajorGcCount(), 1);
+        assertEquals(gcMonitor.getMajorGcTime(), new Duration(3, SECONDS));
+
+        gcMonitor.recordMajorGc(new Duration(7, SECONDS));
+
+        assertEquals(gcMonitor.getMajorGcCount(), 2);
+        assertEquals(gcMonitor.getMajorGcTime(), new Duration(10, SECONDS));
+    }
+}